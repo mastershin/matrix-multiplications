0e4ab296117f73d4cc9a53df953ec1a08c2cfe52
Jae Shin <mastershin@gmail.com>
2024-07-12 22:45:24 -0700

    code format

--- matmul-cuda/main.cpp
+++ matmul-cuda/main.cpp
@@ -30,95 +30,96 @@ A x B = C
 #include "test_matmul.h"
 
 int num_loops = 1;
-#define TOLERANCE 1e-5 // Tolerance for floating-point comparison
+#define TOLERANCE 1e-5  // Tolerance for floating-point comparison
 
 using namespace std;
 using time_point = std::chrono::time_point<std::chrono::high_resolution_clock>;
 
-time_point now() { return std::chrono::high_resolution_clock::now(); }
+time_point now() {
+  return std::chrono::high_resolution_clock::now();
+}
 
 auto time_diff(time_point a, time_point b) {
-    // Return Type: std::chrono::duration<double>
-    return std::chrono::duration_cast<std::chrono::duration<double>>(b - a);
+  // Return Type: std::chrono::duration<double>
+  return std::chrono::duration_cast<std::chrono::duration<double>>(b - a);
 }
 
-void initialize_data(vector<float> &A, vector<float> &B, vector<float> &C,
+void initialize_data(vector<float>& A, vector<float>& B, vector<float>& C,
                      int m, int n, int k) {
-    std::cout << "Data Init Begin" << std::endl;
-    // Matrix Multiplication: A * B = C
+  std::cout << "Data Init Begin" << std::endl;
+  // Matrix Multiplication: A * B = C
 
-    // Calculate the size of matrices A, B, and C
-    int sizeA = m * n;
-    int sizeB = n * k;
-    int sizeC = m * k;
+  // Calculate the size of matrices A, B, and C
+  int sizeA = m * n;
+  int sizeB = n * k;
+  int sizeC = m * k;
 
-    // Initialize matrices A and B with sequential float numbers
-    for (int i = 0; i < sizeA; ++i) {
-        if (i % 2 == 0)
-            A[i] = (float)i; // Assign sequential float values
-        else
-            A[i] = (float)-i;
-    }
+  // Initialize matrices A and B with sequential float numbers
+  for (int i = 0; i < sizeA; ++i) {
+    if (i % 2 == 0)
+      A[i] = (float)i;  // Assign sequential float values
+    else
+      A[i] = (float)-i;
+  }
 
-    for (int i = 0; i < sizeB; ++i) {
-        if (i % 3 == 0)
-            B[i] = 1.0f; // Assign sequential float values
-        else if (i % 3 == 1)
-            B[i] = 2.0f;
-        else
-            B[i] = 3.0f;
-    }
-    std::cout << "Data init done." << std::endl;
+  for (int i = 0; i < sizeB; ++i) {
+    if (i % 3 == 0)
+      B[i] = 1.0f;  // Assign sequential float values
+    else if (i % 3 == 1)
+      B[i] = 2.0f;
+    else
+      B[i] = 3.0f;
+  }
+  std::cout << "Data init done." << std::endl;
 }
 
-void get_small_matrix_size(int &m, int &n, int &k) {
-    // (200x150, 150x100 --> 200x100)
-    // m=200, n=150, k=100 --> sum --> -200 (~1.3 sec on CPU)
-    m = 200;
-    n = 150;
-    k = 100;
+void get_small_matrix_size(int& m, int& n, int& k) {
+  // (200x150, 150x100 --> 200x100)
+  // m=200, n=150, k=100 --> sum --> -200 (~1.3 sec on CPU)
+  m = 200;
+  n = 150;
+  k = 100;
 }
 
-void get_medium_matrix_size(int &m, int &n, int &k) {
-    // 500x300, 300x200, 500x200 --> sum --> -400 (~13 sec on CPU)
-    m = 500;
-    n = 300;
-    k = 200;
+void get_medium_matrix_size(int& m, int& n, int& k) {
+  // 500x300, 300x200, 500x200 --> sum --> -400 (~13 sec on CPU)
+  m = 500;
+  n = 300;
+  k = 200;
 }
 
-void get_large_matrix_size(int &m, int &n, int &k) {
-    // More realistic small-scale LLM size
-    // 4096x1024, 1024x1024, 4096x1024 --> sum --> ?
-    // int m = 4096, n = 1024, k = 1024;
+void get_large_matrix_size(int& m, int& n, int& k) {
+  // More realistic small-scale LLM size
+  // 4096x1024, 1024x1024, 4096x1024 --> sum --> ?
+  // int m = 4096, n = 1024, k = 1024;
 
-    m = 4096;
-    n = 1024;
-    k = 1024;
+  m = 4096;
+  n = 1024;
+  k = 1024;
 }
 
-void get_xl_matrix_size(int &m, int &n, int &k) {
+void get_xl_matrix_size(int& m, int& n, int& k) {
   // 2 billion parameter LLMs
-    m = 4096;
-    n = 8192;
-    k = 16384;
+  m = 4096;
+  n = 8192;
+  k = 16384;
 }
 
-void get_xxl_matrix_size(int &m, int &n, int &k) {
+void get_xxl_matrix_size(int& m, int& n, int& k) {
   // Uses about 1 GB of GPU memory, 256 million x 256 million matrix
-    m = 16384;
-    n = 16384;
-    k = 16384;
+  m = 16384;
+  n = 16384;
+  k = 16384;
 }
 
-
-bool verify_result(const float *C1, const float *C2, int m, int n, int k) {
-    for (int i = 0; i < m * k; ++i) {
-        if (std::fabs(C1[i] - C2[i]) > TOLERANCE) {
-            std::cerr << C1[i] << " vs " << C2[i] << " at i=" << i << std::endl;
-            return false;
-        }
+bool verify_result(const float* C1, const float* C2, int m, int n, int k) {
+  for (int i = 0; i < m * k; ++i) {
+    if (std::fabs(C1[i] - C2[i]) > TOLERANCE) {
+      std::cerr << C1[i] << " vs " << C2[i] << " at i=" << i << std::endl;
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
 // Classic CPU matrix multiplication using for loop (slow)
@@ -126,165 +127,163 @@ bool verify_result(const float *C1, const float *C2, int m, int n, int k) {
 // k) {
 void matmul_cpu(const vector<float> A, const vector<float> B, vector<float> C,
                 int m, int n, int k) {
-    for (int i = 0; i < m; ++i) {
-        for (int j = 0; j < k; ++j) {
-            C.at(i * k + j) = 0.0f;
-            for (int p = 0; p < n; ++p) {
-                C.at(i * k + j) += A[i * n + p] * B[p * k + j];
-            }
-        }
+  for (int i = 0; i < m; ++i) {
+    for (int j = 0; j < k; ++j) {
+      C.at(i * k + j) = 0.0f;
+      for (int p = 0; p < n; ++p) {
+        C.at(i * k + j) += A[i * n + p] * B[p * k + j];
+      }
     }
+  }
 }
 
-void stub_matmul_GPU_float(const float *h_A, const float *h_B, float *h_C,
+void stub_matmul_GPU_float(const float* h_A, const float* h_B, float* h_C,
                            int m, int n, int k) {
-    float *d_A;
-    float *d_B;
-    float *d_C;
-    int sizeA = m * n;
-    int sizeB = n * k;
-    int sizeC = m * k;
-    gpuErrorCheck(cudaMalloc(&d_A, sizeA * sizeof(float)));
-    gpuErrorCheck(cudaMalloc(&d_B, sizeB * sizeof(float)));
-    gpuErrorCheck(cudaMalloc(&d_C, sizeC * sizeof(float)));
+  float* d_A;
+  float* d_B;
+  float* d_C;
+  int sizeA = m * n;
+  int sizeB = n * k;
+  int sizeC = m * k;
+  gpuErrorCheck(cudaMalloc(&d_A, sizeA * sizeof(float)));
+  gpuErrorCheck(cudaMalloc(&d_B, sizeB * sizeof(float)));
+  gpuErrorCheck(cudaMalloc(&d_C, sizeC * sizeof(float)));
 
-    // Copy data to device
-    gpuErrorCheck(
-        cudaMemcpy(d_A, h_A, sizeA * sizeof(float), cudaMemcpyHostToDevice));
-    gpuErrorCheck(
-        cudaMemcpy(d_B, h_B, sizeB * sizeof(float), cudaMemcpyHostToDevice));
+  // Copy data to device
+  gpuErrorCheck(
+      cudaMemcpy(d_A, h_A, sizeA * sizeof(float), cudaMemcpyHostToDevice));
+  gpuErrorCheck(
+      cudaMemcpy(d_B, h_B, sizeB * sizeof(float), cudaMemcpyHostToDevice));
 
-    cuda_matmul_GPU(d_A, d_B, d_C, m, n, k);
-    cudaDeviceSynchronize();
+  cuda_matmul_GPU(d_A, d_B, d_C, m, n, k);
+  cudaDeviceSynchronize();
 
-    // copy from GPU to host
-    gpuErrorCheck(
-        cudaMemcpy(h_C, d_C, sizeC * sizeof(float), cudaMemcpyDeviceToHost));
+  // copy from GPU to host
+  gpuErrorCheck(
+      cudaMemcpy(h_C, d_C, sizeC * sizeof(float), cudaMemcpyDeviceToHost));
 
-    cudaFree(d_A);
-    cudaFree(d_B);
-    cudaFree(d_C);
+  cudaFree(d_A);
+  cudaFree(d_B);
+  cudaFree(d_C);
 }
-void stub_matmul_GPU(vector<float> &A, vector<float> &B, vector<float> &C,
+void stub_matmul_GPU(vector<float>& A, vector<float>& B, vector<float>& C,
                      int m, int n, int k) {
-    float *d_C;
-    gpuErrorCheck(cudaMalloc(&d_C, C.size() * sizeof(float)));
+  float* d_C;
+  gpuErrorCheck(cudaMalloc(&d_C, C.size() * sizeof(float)));
 
-    cuda_matmul_GPU(&A.at(0), &B.at(0), d_C, m, n, k);
-    cudaDeviceSynchronize();
+  cuda_matmul_GPU(&A.at(0), &B.at(0), d_C, m, n, k);
+  cudaDeviceSynchronize();
 
-    // copy from GPU to host
-    gpuErrorCheck(
-        cudaMemcpy(&C.at(0), d_C, C.size() * sizeof(float), cudaMemcpyDeviceToHost));
+  // copy from GPU to host
+  gpuErrorCheck(cudaMemcpy(&C.at(0), d_C, C.size() * sizeof(float),
+                           cudaMemcpyDeviceToHost));
 
-    assert(C.size() == m * k);
-    // C.assign(d_C, d_C + C.size());
-    cudaFree(d_C);
+  assert(C.size() == m * k);
+  // C.assign(d_C, d_C + C.size());
+  cudaFree(d_C);
 }
 
-void _verify_matmul_GPU(vector<float> &A, vector<float> &B, vector<float> &C,
+void _verify_matmul_GPU(vector<float>& A, vector<float>& B, vector<float>& C,
                         int m, int n, int k) {
-    // Allocate memory for matrices A, B, and C
+  // Allocate memory for matrices A, B, and C
 
-    // Initialize vectors a and b
-    initialize_data(A, B, C, m, n, k);
-    vector<float> C_cpu(C.size(), 0);
+  // Initialize vectors a and b
+  initialize_data(A, B, C, m, n, k);
+  vector<float> C_cpu(C.size(), 0);
 
-    // Perform CPU matrix multiplication for verification
-    matmul_cpu(A, B, C_cpu, m, n, k);
+  // Perform CPU matrix multiplication for verification
+  matmul_cpu(A, B, C_cpu, m, n, k);
 
-    stub_matmul_GPU(A, B, C, m, n, k);
+  stub_matmul_GPU(A, B, C, m, n, k);
 
-    if (!verify_result(&C_cpu.at(0), &C.at(0), m, n, k)) {
-        std::cerr << "Verification failed for GPU/CUDA" << std::endl;
-        exit(1);
-    }
+  if (!verify_result(&C_cpu.at(0), &C.at(0), m, n, k)) {
+    std::cerr << "Verification failed for GPU/CUDA" << std::endl;
+    exit(1);
+  }
 
-    std::cout << "Verification Successful for GPU/CUDA" << std::endl;
+  std::cout << "Verification Successful for GPU/CUDA" << std::endl;
 
-    std::cout << "Verification passed" << std::endl;
+  std::cout << "Verification passed" << std::endl;
 }
 
-void do_matmul_GPU(vector<float> &A, vector<float> &B, vector<float> &C, int m,
+void do_matmul_GPU(vector<float>& A, vector<float>& B, vector<float>& C, int m,
                    int n, int k) {
-    // Initialize vectors a and b
-    initialize_data(A, B, C, m, n, k);
+  // Initialize vectors a and b
+  initialize_data(A, B, C, m, n, k);
 
-    auto start_cpu = now();
+  auto start_cpu = now();
 
-    for (int i = 0; i < num_loops; i++) {
-        std::cout << "." << std::flush;
-        stub_matmul_GPU(A, B, C, m, n, k);
-    }
-    auto end_cpu = now();
-    std::chrono::duration<double> duration = end_cpu - start_cpu;
+  for (int i = 0; i < num_loops; i++) {
+    std::cout << "." << std::flush;
+    stub_matmul_GPU(A, B, C, m, n, k);
+  }
+  auto end_cpu = now();
+  std::chrono::duration<double> duration = end_cpu - start_cpu;
 
-    std::cout << std::endl;
-    std::cout << "GPU/CUDA time: " << duration.count() << " seconds"
-              << std::endl;
+  std::cout << std::endl;
+  std::cout << "GPU/CUDA time: " << duration.count() << " seconds" << std::endl;
 }
 
-std::tuple<int, int, int> process_commands(const ArgsMap &args_map) {
-    int m = 0, n = 0, k = 0;
+std::tuple<int, int, int> process_commands(const ArgsMap& args_map) {
+  int m = 0, n = 0, k = 0;
 
-    string matrix_size = args_map.at("size");
+  string matrix_size = args_map.at("size");
 
-    if (matrix_size == "s") {
-        get_small_matrix_size(m, n, k);
-    } else if (matrix_size == "m") {
-        get_medium_matrix_size(m, n, k);
-    } else if (matrix_size == "l") {
-        get_large_matrix_size(m, n, k);
-    } else if (matrix_size == "xl") {
-        get_xl_matrix_size(m, n, k);
-    } else if (matrix_size == "xxl") {
-        get_xxl_matrix_size(m, n, k);
-    } else {
-        die("Invalid size argument. Use 's', 'm', 'l'.");
-    }
+  if (matrix_size == "s") {
+    get_small_matrix_size(m, n, k);
+  } else if (matrix_size == "m") {
+    get_medium_matrix_size(m, n, k);
+  } else if (matrix_size == "l") {
+    get_large_matrix_size(m, n, k);
+  } else if (matrix_size == "xl") {
+    get_xl_matrix_size(m, n, k);
+  } else if (matrix_size == "xxl") {
+    get_xxl_matrix_size(m, n, k);
+  } else {
+    die("Invalid size argument. Use 's', 'm', 'l'.");
+  }
 
-    num_loops = stoi(args_map.at("loop"));
-    cout << "Number of loops: " << num_loops << endl;
+  num_loops = stoi(args_map.at("loop"));
+  cout << "Number of loops: " << num_loops << endl;
 
-    return std::make_tuple(m, n, k);
+  return std::make_tuple(m, n, k);
 }
 
 void check_cuda() {
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    int device_id = 0;
-    cudaError_t cudaStatus;
-    cudaStatus = cudaSetDevice(device_id);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(
-            stderr,
+  // Choose which GPU to run on, change this on a multi-GPU system.
+  int device_id = 0;
+  cudaError_t cudaStatus;
+  cudaStatus = cudaSetDevice(device_id);
+  if (cudaStatus != cudaSuccess) {
+    fprintf(stderr,
             "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        exit(1);
-    }
-    std::cout << "CUDA OK. Using device: " << device_id << std::endl;
+    exit(1);
+  }
+  std::cout << "CUDA OK. Using device: " << device_id << std::endl;
 }
-int main(int argc, char *argv[]) {
-    auto args_map = parse_command_args(argc, argv);
-    auto [m, n, k] = process_commands(args_map);
+int main(int argc, char* argv[]) {
+  auto args_map = parse_command_args(argc, argv);
+  auto [m, n, k] = process_commands(args_map);
 
-    vector<float> A(m * n);
-    vector<float> B(n * k);
-    vector<float> C(m * k);
+  vector<float> A(m * n);
+  vector<float> B(n * k);
+  vector<float> C(m * k);
 
-    std::cout << "Matrix Multiplication: A(" << m << "x" << n << ") * B(" << n
-              << "x" << k << ") = C(" << m << "x" << k << ")" << std::endl;
-    std::cout << "sizeA=" << A.size() << " sizeB=" << B.size()
-              << " sizeC=" << C.size() << std::endl;
+  std::cout << "Matrix Multiplication: A(" << m << "x" << n << ") * B(" << n
+            << "x" << k << ") = C(" << m << "x" << k << ")" << std::endl;
+  std::cout << "sizeA=" << A.size() << " sizeB=" << B.size()
+            << " sizeC=" << C.size() << std::endl;
 
-    //std::cout << "Testing matmul_GPU / CUDA" << std::endl;
-    //test_matmul(stub_matmul_GPU_float);
+  // std::cout << "Testing matmul_GPU / CUDA" << std::endl;
+  // test_matmul(stub_matmul_GPU_float);
 
-    // one last testing with GPU vs CPU results for large matrix
-    // really slow for Large Matrix.
-    //std::cout << "Testing GPU vs CPU result" << std::endl;
-    //_verify_matmul_GPU(A, B, C, m, n, k);
+  // one last testing with GPU vs CPU results for large matrix
+  // really slow for Large Matrix.
+  // std::cout << "Testing GPU vs CPU result" << std::endl;
+  //_verify_matmul_GPU(A, B, C, m, n, k);
 
-    std::cout << "Actual GPU testing..." << std::endl;
-    do_matmul_GPU(A, B, C, m, n, k);
+  std::cout << "Actual GPU testing..." << std::endl;
+  do_matmul_GPU(A, B, C, m, n, k);
 
-    return 0;
+  return 0;
 }
--- matmul-cuda/matmul_gpu.cu
+++ matmul-cuda/matmul_gpu.cu
@@ -1,29 +1,28 @@
+#include <numeric>  // std::accumulate
+
 #include "matmul_gpu.h"
-#include <numeric> // std::accumulate
 
 // Function to convert a row-major 1D array to a column-major 1D array
-void _transpose(const float* row_major, float* column_major, int rows, int cols)
-{
-    for (int i = 0; i < rows; ++i)
-    {
-        for (int j = 0; j < cols; ++j)
-        {
-            column_major[j * rows + i] = row_major[i * cols + j];
-        }
+void _transpose(const float *row_major, float *column_major, int rows,
+                int cols) {
+  for (int i = 0; i < rows; ++i) {
+    for (int j = 0; j < cols; ++j) {
+      column_major[j * rows + i] = row_major[i * cols + j];
     }
+  }
 }
 
 // GPU / CUDA matrix multiplication
 __global__ void cuda_matmul_GPU(const float *A, const float *B, float *C, int m,
-                           int n, int k) {
-    int row = blockIdx.y * blockDim.y + threadIdx.y;
-    int col = blockIdx.x * blockDim.x + threadIdx.x;
+                                int n, int k) {
+  int row = blockIdx.y * blockDim.y + threadIdx.y;
+  int col = blockIdx.x * blockDim.x + threadIdx.x;
 
-    if (row < m && col < k) {
-        float sum = 0.0f;
-        for (int i = 0; i < n; ++i) {
-            sum += A[row * n + i] * B[i * k + col];
-        }
-        C[row * k + col] = sum;
+  if (row < m && col < k) {
+    float sum = 0.0f;
+    for (int i = 0; i < n; ++i) {
+      sum += A[row * n + i] * B[i * k + col];
     }
+    C[row * k + col] = sum;
+  }
 }
