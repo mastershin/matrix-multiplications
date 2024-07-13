6b4aac28c47449203ef3934ae06b7d402e5b3789
Jae <mastershin@gmail.com>
2024-07-13 08:29:38 -0700

    loop to 200, update performances

--- matmul-cuda/run-gpu-cuda.sh
+++ matmul-cuda/run-gpu-cuda.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-./matmul_gpu.ex --size s --loop 100
+./matmul_gpu.ex --size s --loop 200
 
-./matmul_gpu.ex --size m --loop 100
+./matmul_gpu.ex --size m --loop 200
 
-./matmul_gpu.ex --size l --loop 100
+./matmul_gpu.ex --size l --loop 200
 
-./matmul_gpu.ex --size xl --loop 100
+./matmul_gpu.ex --size xl --loop 200
 
-./matmul_gpu.ex --size xxl --loop 100
+./matmul_gpu.ex --size xxl --loop 200
